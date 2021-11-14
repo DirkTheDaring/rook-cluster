9a64728941bfc535ca7508ea150e812361929ecd
Dietmar Kling <dietmar.kling@googlemail.com>
2021-11-14 09:29:43 +0100

    change rook-argocd to helm as we depend on rook to provide ha for argo

---
+++ roles/rook-argocd/bin/get-release-versions.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/bash
+# JMESPATH:
+# pip install jmespath-terminal
+# JQUERY
+# sudo dnf install -y jq
+set -e
+REPO_NAME="rook/rook"
+URL="https://api.github.com/repos/$REPO_NAME/releases"
+
+CACHE_DIR="$HOME/.cache/releases/$REPO_NAME"
+CACHE_FILE="$CACHE_DIR/json"
+
+[ -d "$CACHE_DIR" ] || mkdir -p "$CACHE_DIR"
+
+if [ ! -f "$CACHE_FILE"  ]; then
+  curl -o "$CACHE_FILE" -L "$URL"
+fi
+
+VERSION=$(cat $CACHE_FILE | jq -r ".[0].name")
+echo $VERSION
+if [ "$1" == "-d" ]; then 
+  URL=$(cat $CACHE_FILE | jq -r ".[0].tarball_url" )
+  curl --silent -OL --remote-header-name "$URL"
+fi
