773b5dab959729c29177bc0c40ae0048c4fb3cb7
Dietmar Kling <dietmar.kling@googlemail.com>
2021-12-02 11:32:49 +0100

    add mount again

---
+++ bin/mount_cephfs.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+#set -x
+if [ "$1" == "" ]; then
+  echo "need rook/config.json"
+  exit 1
+fi 
+if [ "$2" == "" ]; then
+  echo "need mountpoint. e.g. /opt/ceph"
+  exit 2
+fi 
+JSON=$(cat credentials/fritz.box/storage1/rook/config.json)
+ROOK_EXTERNAL_CEPH_MON_DATA=$(jq -r ".rook_external_ceph_mon_data" <<< "$JSON")
+ROOK_EXTERNAL_CEPH_MON_DATA=${ROOK_EXTERNAL_CEPH_MON_DATA//[a-c]=/}
+ROOK_EXTERNAL_ADMIN_SECRET=$(jq -r ".rook_external_admin_secret" <<<"$JSON")
+
+mkdir -p "$MNT"
+sudo mount -t ceph -o mds_namespace=ceph-filesystem,name=admin,secret=$ROOK_EXTERNAL_ADMIN_SECRET $ROOK_EXTERNAL_CEPH_MON_DATA:/ "$MNT"
